99825f2be662a2de804c859660c89284e58c2ce5
ByBresh <contact@bybresh.com>
2025-11-10 20:16:29 +0100

    Make DB container persistent and wait for the database before running services

--- OrderFlow/AppHost.cs
+++ OrderFlow/AppHost.cs
@@ -1,9 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("postgres");
+// Configure PostgreSQL database
+var postgres = builder.AddPostgres("postgres")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+// Add a database to the PostgreSQL instance
 var postgresdb = postgres.AddDatabase("postgresdb");
 
+// Add the OrderFlow services
 builder.AddProject<Projects.OrderFlow_Identity>("orderflow-identity")
+    .WaitFor(postgres)
     .WithReference(postgresdb);
 
 builder.Build().Run();

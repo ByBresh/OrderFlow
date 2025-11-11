1519bfab67a8e31d1503b37d295e72260d345dd1
ByBresh <contact@bybresh.com>
2025-11-11 15:54:10 +0100

    Changed identitydb name

--- OrderFlow/AppHost.cs
+++ OrderFlow/AppHost.cs
@@ -1,15 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 // Configure PostgreSQL database
-var postgres = builder.AddPostgres("postgres")
+var postgres = builder.AddPostgres("identity")
     .WithLifetime(ContainerLifetime.Persistent);
 
 // Add a database to the PostgreSQL instance
-var postgresdb = postgres.AddDatabase("postgresdb");
+var identitydb = postgres.AddDatabase("indentitydb");
 
 // Add the OrderFlow services
 builder.AddProject<Projects.OrderFlow_Identity>("orderflow-identity")
     .WaitFor(postgres)
-    .WithReference(postgresdb);
+    .WithReference(identitydb);
 
 builder.Build().Run();

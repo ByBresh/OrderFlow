69975b39c814f77cff48ec99e5d10d53e642e28e
ByBresh <contact@bybresh.com>
2025-11-11 16:46:06 +0100

    Fixed spelling error and connected react frontent to Aspire

--- OrderFlow/AppHost.cs
+++ OrderFlow/AppHost.cs
@@ -5,11 +5,18 @@ var postgres = builder.AddPostgres("identity")
     .WithLifetime(ContainerLifetime.Persistent);
 
 // Add a database to the PostgreSQL instance
-var identitydb = postgres.AddDatabase("indentitydb");
+var identitydb = postgres.AddDatabase("identitydb");
 
 // Add the OrderFlow services
-builder.AddProject<Projects.OrderFlow_Identity>("orderflow-identity")
+var identityApi = builder.AddProject<Projects.OrderFlow_Identity>("orderflow-identity")
     .WaitFor(postgres)
     .WithReference(identitydb);
+
+// Add the React frontend
+var frontend = builder.AddNpmApp("orderflow-front", "../orderflow.front")
+    .WithReference(identityApi)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
 builder.Build().Run();
